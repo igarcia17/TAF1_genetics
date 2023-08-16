d81f880cff9fd6c1abd834ec511bd3f78006b100
igarcia17 <ines.garciaortiz99@gmail.com>
2023-08-16 17:21:19 +0200

    gsea done

--- GSEA.R
+++ GSEA.R
@@ -9,7 +9,11 @@ suppressPackageStartupMessages({
   library(UpSetR, quietly = TRUE)
   library(enrichplot, quietly = TRUE)
   library(ggplot2)
+  library(data.table)
 })
+#Paths
+workingD <- rstudioapi::getActiveDocumentContext()$path
+setwd(dirname(workingD))
 
 #Parameters
 #Which database inside msigdbr?
@@ -17,24 +21,18 @@ category <- 'C2'
 subcategory <- 'REACTOME'
 #msigdbr_collections()
 
-#Will you use stat parameter for ordering or the shrinked log2fold change?
-#Log2fold is traditional statistic, but lately t statistic has been more recommended
-#https://www.biorxiv.org/content/10.1101/060012v3.full.pdf
-statP <- F
-#Plot the x top categories
 topCat <- 15
 
-#Paths
-workingD <- rstudioapi::getActiveDocumentContext()$path
-setwd(dirname(workingD))
+
 #Input
-experiment <- 'Salmon_Ribominus'
+experiment <- 'Salmon_polyA'
 input <- paste0('DeSeq_results/',experiment, '.txt')
 
 #Outputs
 resD0 <- paste0('GSEA_results/', experiment, '/')
 
 resD <- gsub(':','_',paste0(resD0,category,'_', subcategory, '/'))
+#resD <- paste0(dirname(workingD), resD)
 if (!file.exists(resD)){
   dir.create(file.path(resD))
 }
@@ -46,32 +44,36 @@ ridgeF <- paste0(resD,"GSEA_ridge.jpeg")
 upsetF <- paste0(resD,"upset_plot.jpeg")
 gseaplotsF <- paste0(resD,'all_gseaplots.jpeg')
 
-library(data.table)
+
 #1) Load data
 data <- fread(input)
+#duplicates_count <- data %>% group_by(ENSID) %>% summarise(count = n())
+
 #I am going to work over the stat parameter. 
 #To calculate this i divide the log2FoldChange by the lfcSE.
 #The other option, using log2FoldChange, would require shrinkage
-#As the data is not a DESEqDataSet object, I cannot do the shrinkage
+#As the data is not a DESEqDataSet object, I cannot do the shrinkage. 
+#Stat not always available, check input
+original_gene_list <- data$stat
+names(original_gene_list)<- data$GENE_ID
+gene_list <- na.omit(original_gene_list)
+gene_list <- sort(gene_list, decreasing=T)
 
-dat <- data$stat
-names(dat) <- as.character(data$ENSID)
 # Remove items with name '0'
-dat <- dat[names(dat) != "0"]
-dat <- sort(dat, decreasing=TRUE)
+#dat <- dat[names(dat) != 0,] #only human, discuss how to handle duplicates
+#dat <- sort(dat$Stat, decreasing=TRUE)
 
 #2) Calculate GSEA and write tables of results
 #Get genes and categories
-db_sets <- msigdbr(species = 'Homo sapiens', category = category, 
+db_sets <- msigdbr(species = 'Mus musculus', category = category, 
                    subcategory = subcategory)%>% 
   dplyr::select(gs_name, ensembl_gene)
 head(db_sets) #each gene associated with each msig group
 
 #3) Perform GSEA
 set.seed(1)
-egs <- GSEA(geneList = dat, pvalueCutoff = 0.05, eps = 0.01, pAdjustMethod = "BH", 
+egs <- GSEA(geneList = gene_list, pvalueCutoff = 0.05, eps = 0, pAdjustMethod = "BH", 
             seed = T, TERM2GENE = db_sets) #for more accurate p value set eps to 0
-#https://bioconductor.org/packages/release/bioc/vignettes/fgsea/inst/doc/fgsea-tutorial.html 
 #head(egs@result)
 egs_df <- data.frame(egs@result)
 egs_df <- egs_df[, -c(1,2)]
@@ -126,25 +128,6 @@ for (gene in 1:length(uniq)) {
     if (gen %in% list_top[[func]]) {
       mat[gene,func] =  1
     }}} 
-
-#Make a data frame
-mat_df <- as.data.frame(mat)
-colnames(mat_df) <- func_top
-row.names(mat_df) <- uniq
-
-#Plot
-jpeg(file = upsetF, units = 'in', width = 15, height = 10, res = 300)
-upset(mat_df, nsets=10, order.by="freq", sets.bar.color="skyblue")
-invisible(dev.off())
-
-###Plot the first 5 more abundant terms or all if there are less hits
-
-for (j in 1:topCat){
-  plot <- gseaplot2(egs, geneSetID=j, title = egs$Description[j], base_size=40, color="red")
-  desc <- gsub(" ", "_", egs$Description[j], fixed = TRUE) 
-  filename <- paste0(resD, desc, ".jpeg")
-  ggsave(plot, file=filename, device = "jpeg", units= "in", height = 15, width = 20)
-}
 
 #For all top categories at once:
 gseap <- gseaplot2(egs, geneSetID = 1:topCat, pvalue_table = F)
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Genetics analysis related to TAF1 and their interactors related to MS.
 1. GSEA RNAseq data
    From two DeSeq results, a) Ribominus and b) PolyA, both aligned using Salmon.
-   Use all databases
+   Use all databases. I have run it on Mus musculus data base, for human tell me how to handle duplicates on ENSID.
 2. Gene-set analysis of MAGMA for in silico validation of GSEA results. Check for MS, esophageal-gastric cancer and ALS.
-3. GO categories of interactors in column O, sheet DATA of file 'interactores_and_other_data.xlsx' -> hago gene set basandome en estas categorias o que tenia que hacer?
-4. [DONE] Gene-set analysis of MAGMA for those interactors, 4 sets: only WT, only mut, not differential, all together.
+3. GO categories of interactors in column O, sheet DATA of file 'interactores_and_other_data.xlsx' and gene-set analysis of emerged categories
+4. [DONE] Gene-set analysis of MAGMA for those interactors, 4 sets: only WT, only mut, not differential, all together. Found in column O.
 5. ORA with oligodendrocytes markers over the genes on the CHIP-seq pause peak.
